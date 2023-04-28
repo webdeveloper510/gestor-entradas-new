232dde60b18a76c27711e373b2bef671a86f388b
Codenomad <webdeveloper510>
2023-04-28 14:57:20 +0530

    new table

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template, request, redirect, session, abort, flash, send_file, session,url_for
 from db import app,db
-from models import User, Cliente
+from models import User, Cliente, Discoteca
 from passlib.apps import custom_app_context as pwd_context
 
 
@@ -52,7 +52,8 @@ def login():
             print(e)
         if passs == True:
             session['userid'] = user.id 
-            session['firstname'] = user.firstname             
+            session['firstname'] = user.firstname     
+            session['role'] = user.role      
             return "Successfully"
         else:
             return "Incorrect email or password"    
@@ -64,6 +65,31 @@ def login():
 def dashboard():
    return render_template('index3.html')
 
+
+
+def obtener_discotecas():
+   discotecas = []
+   try:
+      consulta = Discoteca.query.all()
+      print(consulta)
+      
+      for disco in consulta:
+         id=disco.id
+         nombre=disco.nombre
+         lugar=disco.lugar
+         aforo=disco.aforo
+         desc=disco.descripcion
+         discotecas.append({'id':id,'nombre':nombre,'lugar':lugar,'aforo':aforo,'descripcion':desc})
+      # Cerramos el cursor y la conexión a la base de datos
+     
+   except ValueError as e:
+      return -1
+   return discotecas
+
+@app.route('/discoteacachoices')
+def discotecachoices():
+    discotecas = obtener_discotecas()
+    return render_template('discotecachoices.html',discotecas=discotecas)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import ForeignKey
+from datetime import datetime
 db = SQLAlchemy()
 
 
@@ -12,8 +12,8 @@ class User(db.Model):
     email = db.Column(db.String(500))
     password = db.Column(db.String(500))
     role = db.Column(db.String(500))
-    created = db.Column(db.DateTime)
-    updated = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime,default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime,default=datetime.utcnow)
     
 
     
@@ -27,13 +27,35 @@ class Cliente(db.Model):
     apellidos = db.Column(db.String(500))
     correo = db.Column(db.String(500))
     fecha_nacimiento = db.Column(db.DateTime)
-    created = db.Column(db.DateTime)
-    updated = db.Column(db.DateTime)
+  
+    
+    
+    
+class Discoteca(db.Model):
+    __tablename__ = 'discoteca'
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(500))
+    lugar = db.Column(db.String(500))
+    aforo = db.Column(db.Integer)
+    descripcion = db.Column(db.Text)    
+  
+ 
+class Evento(db.Model):
+    __tablename__ = 'evento'
+    id = db.Column(db.Integer, primary_key = True)
+    id_disco = db.Column(db.Integer, ForeignKey('discoteca.id'))  
+    nombre = db.Column(db.String(500))
+    descripcion = db.Column(db.Text)
+    aforo = db.Column(db.Integer)
+    fecha = db.Column(db.DateTime,default=datetime.utcnow)
+    hora = db.Column(db.DateTime,default=datetime.utcnow)
+    aforo = db.Column(db.Integer,default=18)
+    
     
     
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
-# db.create_all()
+
     
     
     
